2d4f093fd6fc9c0a872044b86fa4f5ab0ebc60c5
razz <rpruteanu@gmail.com>
2020-10-16 19:22:06 +0300

    16Oct19:22

---
+++ days/19-21-itertools/yo.py
@@ -0,0 +1,19 @@
+### Iteration Refresher
+
+# number = list(range(1, 11))
+
+## Running in the shell the fol. will dem. that it is an iterable:
+# print('__iter__' in dir('number'))
+
+# it = iter('string')
+# print(next(it))
+# print(next(it))
+# print(next(it))
+# print(next(it))
+# print(next(it))
+# print(next(it))
+# print(next(it))
+
+## This will not throw and error when it gets to the end
+# for char in 'string':
+#     print(char)
