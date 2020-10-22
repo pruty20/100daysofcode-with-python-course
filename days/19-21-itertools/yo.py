925ec3e2210c9a820f1b65cc1cb9987eb97c3cec
RAZZ <rpruteanu@gmail.com>
2020-10-22 21:40:08 +0300

    22Oct2140

--- days/19-21-itertools/yo.py
+++ days/19-21-itertools/yo.py
@@ -17,3 +17,13 @@
 ## This will not throw and error when it gets to the end
 # for char in 'string':
 #     print(char)
+
+### Itertools - Cycle
+import itertools, sys, time
+
+symbols = itertools.cycle('-\|/')
+
+while True:
+    sys.stdout.write('\r' + next(symbols)) # '\r' -- will make sure that the iteritems will be displayed on the same line
+    sys.stdout.flush()
+    time.sleep(0.3)
