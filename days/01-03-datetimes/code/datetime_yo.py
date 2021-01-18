64ea0204ca4ddd181d7fd523a0703c4002f719a4
RAZZ <rpruteanu@gmail.com>
2021-01-18 23:35:55 +0200

    18Jan23:35

--- days/01-03-datetimes/code/datetime_yo.py
+++ days/01-03-datetimes/code/datetime_yo.py
@@ -6,19 +6,49 @@ from datetime import date
 # today = datetime.today()
 # print(type(today))
 
-todaydate = date.today()
-# print(type(todaydate))
-# print(todaydate)
-# print(todaydate.month)
-# print(todaydate.day)
-# print(todaydate.year)
+# todaydate = date.today()
+# # print(type(todaydate))
+# # print(todaydate)
+# # print(todaydate.month)
+# # print(todaydate.day)
+# # print(todaydate.year)
 
-christmas = date(2021, 12, 25)
-print(christmas)
-print(christmas-todaydate)
-print((christmas-todaydate).days)
+# christmas = date(2021, 12, 25)
+# print(christmas)
+# print(christmas-todaydate)
+# print((christmas-todaydate).days)
 
-if christmas != todaydate:
-    print("Sorry, there are still " + str((christmas-todaydate).days) + " days until Christmas")
-else:
-    print("Yay, it's Christmas")
+# if christmas != todaydate:
+#     print("Sorry, there are still " + str((christmas-todaydate).days) + " days until Christmas")
+# else:
+#     print("Yay, it's Christmas")
+
+### Time delta
+from datetime import timedelta
+
+t = timedelta(days=4, hours=10)
+# print(type(t))
+print(t.days)
+print(t.seconds) ## its only able to show seconds for one full day e.g: hours=23, minultes=59, seconds=59
+print(t.seconds / 60 / 60) ## will convert to hours OR t.seconds / 3600  /// t.hours --> will fail
+
+eta = timedelta(hours=6)
+today = datetime.today()
+print(today)
+print(eta)
+print(today + eta)
+print(str(today + eta))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
