314668e344a0fd6657afac0b842d2df1b9bdcfc3
RAZZ <rpruteanu@gmail.com>
2021-02-10 21:57:21 +0200

    10Feb21:57

--- days/22-24-decorators/decorators_yo.py
+++ days/22-24-decorators/decorators_yo.py
@@ -23,3 +23,44 @@ def my_function(args):
 # def my_function(args):
 #     pass
 # my_function = mydecorator(my_function)
+
+"""
+About args and kwargs functions
+"""
+# def get_profile(name, active=True, *sports, **awards):
+#     print('Positional arguments (required): ', name)
+#     print('Keyword arguments (not required, default values): ', active)
+#     print('Arbitrary argument list (sports): ', sports)
+#     print('Arbitrary keyword argument dictionary (awards): ', awards)
+
+# print(get_profile())  # will return an error since 'name' is a positional argument
+# print(get_profile('Razvan'))
+# print(get_profile('Razvan', active=False))
+# print(get_profile('Razvan', active=False, 'basketball', 'soccer'))  # will return an error since the system thinks a dictionary is being passed but it can be used as # print(get_profile('Razvan', False, 'basketball', 'soccer'))
+# print(get_profile('Razvan', False, 'basketball', 'soccer',
+#                   pythonista='special honor of the community', topcoder='2017 code camp'))
+
+
+"""
+using decorators
+"""
+def show_args(function):
+    @wraps(function)
+    def wrapper(*args, **kwargs):
+        print('hi from decorator - args:')
+        print(args)
+        result = function(*args, **kwargs)
+        print('hi again from decorator - kwargs:')
+        print(kwargs)
+        return result
+    # return wrapper as a decorated function
+    return wrapper
+
+@show_args
+def get_profile(name, active=True, *sports, **awards):
+    print('\n\thi from get_profile function\n')
+
+get_profile('bob', True, 'basketball', 'soccer',
+            pythonista='special honor of the community', topcoder='2017 code camp')
+
+
