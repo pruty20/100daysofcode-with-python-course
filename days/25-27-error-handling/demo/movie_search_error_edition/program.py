d3198a1581e7ae0e0471d502cf654c84ebbbd70a
RAZZ <rpruteanu@gmail.com>
2021-02-14 01:21:06 +0200

    14Feb01:20

--- days/25-27-error-handling/demo/movie_search_error_edition/program.py
+++ days/25-27-error-handling/demo/movie_search_error_edition/program.py
@@ -1,22 +1,35 @@
-import api
-import requests.exceptions
+# import api
+# import requests.exceptions
+#
+#
+# def main():
+#     keyword = input('Keyword of title search: ')
+#     try:
+#         results = api.find_movie_by_title(keyword)
+#
+#         print(f'There are {len(results)} movies found.')
+#         for r in results:
+#             print(f"{r.title} with code {r.imdb_code} has score {r.imdb_score}")
+#     except requests.exceptions.ConnectionError:
+#         print("ERROR: Could not find server. Check your network connection.")
+#     except ValueError:
+#         print("ERROR: You must specify a search term.")
+#     except Exception as x:
+#         print("Oh that didn't work!: {}".format(x))
 
+
+"""
+Raw version of the above:
+"""
+import api
 
 def main():
     keyword = input('Keyword of title search: ')
-    try:
-        results = api.find_movie_by_title(keyword)
+    results = api.find_movie_by_title(keyword)
 
-        print(f'There are {len(results)} movies found.')
-        for r in results:
-            print(f"{r.title} with code {r.imdb_code} has score {r.imdb_score}")
-    except requests.exceptions.ConnectionError:
-        print("ERROR: Could not find server. Check your network connection.")
-    except ValueError:
-        print("ERROR: You must specify a search term.")
-    except Exception as x:
-        print("Oh that didn't work!: {}".format(x))
-
+    print(f'There are {len(results)} movies found')
+    for r in results:
+        print(f'{r.title} with code {r.imdb_code} has score {r.imdb_score}')
 
 if __name__ == '__main__':
     main()
